bd46d529466ccad1ebdc06728aea7971a22fe560
Joao Henrique <joaohenriquefsf@gmail.com>
2024-09-25 16:53:28 -0300

    feat: docstring and type hint into partsers

--- backend/backend/parsers/csv_parser.py
+++ backend/backend/parsers/csv_parser.py
@@ -1,6 +1,8 @@
 import csv
 
-def parse_csv(file_path):
+def parse_csv(file_path: str) -> list:
+    """Parse a CSV file and return a list of dictionaries."""
+    
     with open(file_path, mode='r', encoding='utf-8') as file:
         csv_reader = csv.DictReader(file)
         return [row for row in csv_reader]
--- backend/backend/parsers/json_parser.py
+++ backend/backend/parsers/json_parser.py
@@ -1,5 +1,7 @@
 import json
 
-def parse_json(file_path):
+def parse_json(file_path: str) -> dict:
+    """Parse a JSON file and return a dictionary."""
+    
     with open(file_path, mode='r', encoding='utf-8') as file:
         return json.load(file)
